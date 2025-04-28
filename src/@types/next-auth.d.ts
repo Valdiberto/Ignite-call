f5f444ceb0a63b63b850f59e368700901c7b39b7
Valdiberto <junior-dk555@hotmail.com>
2025-04-28 15:51:30 -0300

    trying to fix  array.map( nom empty string

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -8,10 +8,7 @@ declare module 'next-auth' {
     avatar_url: string
   }
 
-  
   interface Session {
     user: User
   }
-
-  
 }
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -6,18 +6,20 @@ export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   const weekdays = Array.from(Array(7).keys())
-    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
-    .map((weekDay) => {
+    .map((day) => {
+      const weekDay = formatter.format(new Date(Date.UTC(2021, 5, day)))
       if (!weekDay || weekDay.trim() === '') {
-        throw new Error('Returned weekday is empty')
+        console.error('Empty string encountered for weekday:', day)
+        return '' // Garantir que não passaremos um valor vazio
       }
-
-      if (short) {
-        return weekDay.substring(0, 3).toUpperCase()
-      }
-
-      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+      return weekDay
     })
+    .filter((weekDay) => weekDay !== '') // Filtra strings vazias
 
-  return weekdays
+  return weekdays.map((weekDay) => {
+    if (short) {
+      return weekDay.substring(0, 3).toUpperCase()
+    }
+    return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+  })
 }
